7ecfa8076a44093925de1b1369b32cedeb5fb502
ryutah <6662577+ryutah@users.noreply.github.com>
2021-07-17 20:55:28 +0900

    feat: wip run database in docker container

---
+++ scripts/env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export GOOGLE_CLOUD_PROJECT="mwwolf_local"
+export DATASTORE_EMULATOR_HOST=localhost:61000
---
+++ scripts/run_database.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker container run -d --net host --rm docker.pkg.github.com/mwwolf/mwwolf/datastore:1.0.2
